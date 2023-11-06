93fe1cae482b86b9b1ce05e06547928e1801d0ee
Jenn <jenniferjuarezjj@gmail.com>
2023-11-06 17:00:10 -0500

    create ProductController

--- main/java/com/jenncodes/InventoryManagement/controller/ProductController.java
+++ main/java/com/jenncodes/InventoryManagement/controller/ProductController.java
@@ -1,4 +1,42 @@
 package com.jenncodes.InventoryManagement.controller;
 
+import com.jenncodes.InventoryManagement.model.Product;
+import com.jenncodes.InventoryManagement.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    // Create a new product
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product newProduct = productService.addProduct(product);
+        return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+    }
+
+    // Update an existing product
+    @PutMapping("/{productId}")
+    public ResponseEntity<Product> updateProduct(
+            @PathVariable Long productId,
+            @RequestBody Product updatedProduct) {
+        Product updated = productService.updateProduct(productId, updatedProduct);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+
+    // Retrieve a product by ID
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long productId) {
+        Product product = productService.getProductById(productId);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    // Add more endpoints for additional functionality, such as checking stock levels, notifications, etc.
 }
+
